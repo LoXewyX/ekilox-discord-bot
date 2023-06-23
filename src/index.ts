29c0e3c4b290007ea161e94ace52e8a5ddd97946
Ekilox <86871709+LoXewyX@users.noreply.github.com>
2023-06-24 01:11:25 +0200

    Updated some things

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import express from "express";
-import serverless from "serverless-http";
 import { createApiRest } from "./api-rest";
 import { client } from "./bot";
 
-const app = express();
-
-// Create the API routes
 const api = createApiRest(client);
-app.use("/api", api);
+const port = process.env.PORT || 3000;
 
-// Define additional routes or middleware as needed
-
-// Create the serverless handler
-const handler = serverless(app);
-
-// Export the handler for serverless deployment
-export { handler };
+api.listen(port, () => {
+  console.log(`[ekilox-api] Server is running on port ${port} ⚡`);
+});
